ebb93636b21222e946684ff9402b404b29563dd4
CarrotAnikerok <vika.koreckina@yandex.ru>
2022-04-08 18:53:55 +0300

    third commit

--- Задачи 2 семестра/OOP/regular_phrasing.py
+++ Задачи 2 семестра/OOP/regular_phrasing.py
@@ -20,13 +20,13 @@ def number(a):
     for m in re.finditer(sample, a):
         print(m)
     #comp_sample = re.compile(sample)
-    #print(comp_sample.findall(a))
-    #print(re.findall(sample, a))
+    #print(comp_sample.findall(a))?
+    #print(re.findall(sample, a))?
 
 s = "Привет привет как дела я не знаю о смотри +34578128 даааа вот так вот о смотри еще есть 456627-3 5-6 такие вот номера да а еще +8 (913)-45-64 ну и 46"
 number(s)
 
-#task3
+#task 3
 
 def spaces(a):
     sample = r"\s+,"
@@ -37,4 +37,26 @@ def spaces(a):
         a = a.replace(i, ",")
     return a
 
-print(spaces(("Привет    , друг")))
+print(spaces(("Привет    , друг  , мой друг")))
+
+#task 4
+
+def backward(a):
+    sample = r"(A-Za-z)+-(A-Za-z)+"
+    return re.findall(sample, a) #не работает
+
+print(backward("Wha-aaaat are yo-ou doing?"))
+
+#task 5
+
+def cat(a):
+    sample = r"(^|\s)*(К|к)(О|о)(Т|т)($|\s)+"
+    n =0
+    for m in re.finditer(sample, a):
+        n+=1
+    return(n)
+
+print(cat("КоТ а еще есть который кот и конечно Кот же еще некоторый кот кот"))
+
+
+
