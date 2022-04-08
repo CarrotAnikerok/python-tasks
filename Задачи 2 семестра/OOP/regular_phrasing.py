4bb491b4211718c12c7e02d38fde5dc06169e8ad
CarrotAnikerok <vika.koreckina@yandex.ru>
2022-04-08 18:02:50 +0300

    second commit

--- Задачи 2 семестра/OOP/regular_phrasing.py
+++ Задачи 2 семестра/OOP/regular_phrasing.py
@@ -17,12 +17,24 @@ right_email(line)
 
 def number(a):
     sample = r'\+?([-\s()]*\d){7,11}'
-    comp_sample = re.compile(sample)
-    print(comp_sample.findall(a))
+    for m in re.finditer(sample, a):
+        print(m)
+    #comp_sample = re.compile(sample)
+    #print(comp_sample.findall(a))
+    #print(re.findall(sample, a))
 
 s = "Привет привет как дела я не знаю о смотри +34578128 даааа вот так вот о смотри еще есть 456627-3 5-6 такие вот номера да а еще +8 (913)-45-64 ну и 46"
 number(s)
 
+#task3
 
+def spaces(a):
+    sample = r"\s+,"
+    for m in re.finditer(sample, a):
+        print(m)
+    f = re.findall(sample,a)
+    for i in f:
+        a = a.replace(i, ",")
+    return a
 
-
+print(spaces(("Привет    , друг")))
